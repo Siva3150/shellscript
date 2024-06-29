b9a8f8c0f2c6275dc22ed470205e06f9dbb093bb
siva3150 <siva123@gmail.com>
2024-06-29 13:13:37 +0530

    shell-script

--- 11-arguments.sh
+++ 11-arguments.sh
@@ -5,8 +5,10 @@ NUMBER2=$2
 
 SUM=$(($NUMBER1+$NUMBER2))
 
-echo "Total:: $SUM "
+echo "Total:: $SUM"
 
-echo "Total arguments:: $@ "
+echo "Total arguments:: $@"
 
-echo "How many arguments passed: $@ "
+echo "How many arguments passed: $@"
+
+echo "script name:: $0"
