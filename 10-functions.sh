1cbd92150625f65dec237bd09e40e3641956449a
siva3150 <siva123@gmail.com>
2024-06-29 12:41:59 +0530

    shell-script

---
+++ 10-functions.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+ID=$(id -u)
+
+VALIDATE(){
+ if [ $? - ne 0 ]
+ then
+    echo "Error::  installation is failed"
+    exit 1
+ else
+    echo " installation is success"
+ fi
+}
+
+if [ $ID - ne 0 ]
+then 
+    echo "Error:: Run the system with root user"
+    exit 1 #you can give other than 0
+else
+    echo "You are root user"
+fi #fi means reverse of if, means condition end
+
+yum install mysql -y
+
+VALIDATE
+
+yum install GIT -y
+
+VALIDATE
