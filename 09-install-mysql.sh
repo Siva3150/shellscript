fb15ea612b26347296b3db16e617eecacf16b000
siva3150 <siva123@gmail.com>
2024-06-29 12:03:16 +0530

    shell-script

--- 09-install-mysql.sh
+++ 09-install-mysql.sh
@@ -11,3 +11,11 @@ else
 fi #fi means reverse of if, means condition end
 
 yum install mysql -y
+
+if [ $? - ne 0 ]
+then
+  echo "Error:: Mysql installation is failed"
+  exit 1
+else
+  echo "Mysql installation is success"
+fi
